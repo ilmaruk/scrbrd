4a98c14a821b6d6c61d874acc36efc5f316fccee
Ruggero Ferretti <ruggero.ferretti@audioanalytic.com>
2022-10-10 09:42:20 +0200

    Using boto3 resource.

--- backend/lambdas/ws_endpoint.py
+++ backend/lambdas/ws_endpoint.py
@@ -1,27 +1,26 @@
 import boto3
 import os
 
-dynamodb = boto3.client("dynamodb")
+dynamodb = boto3.resource("dynamodb")
 
 
 def lambda_handler(event, context):
-    print("EVENT", event)
-    print("CONTEXT", context)
+    # print("EVENT", event)
+    # print("CONTEXT", context)
 
     request_context = event.get("requestContext", {})
 
     route_key = request_context.get("routeKey")
     connection_id = request_context.get("connectionId")
 
+    connections_table = dynamodb.Table(os.environ.get("CONNECTIONS_TABLE"))
+
     if route_key == "$connect":
-        dynamodb.put_item(
-            TableName=os.environ.get("CONNECTIONS_TABLE"),
-            Item={"connectionId": {"S": connection_id}}
-        )
+        connections_table.put_item(Item={"connectionId": connection_id})
     elif route_key == "$disconnect":
-        dynamodb.delete_item(
-            TableName=os.environ.get("CONNECTIONS_TABLE"),
-            Key={"connectionId": {"S": connection_id}}
-        )
+        connections_table.delete_item(Key={"connectionId": connection_id})
+    else:
+        # TODO: return an error
+        pass
 
     return {}
