642575c81b471f62230144380424fba574097d09
Ruggero Ferretti <ruggero.ferretti@audioanalytic.com>
2022-10-12 10:28:52 +0200

    Processing SCORE events.

--- backend/lambdas/events_processor.py
+++ backend/lambdas/events_processor.py
@@ -1,8 +1,10 @@
 import json
-import boto3
 import os
 
-dynamodb = boto3.client("dynamodb")
+import boto3
+from boto3.dynamodb.types import TypeDeserializer
+
+dynamodb = boto3.resource("dynamodb")
 
 
 def lambda_handler(event, context):
@@ -12,7 +14,26 @@ def lambda_handler(event, context):
     if len(records) == 0:
         return {}
 
-    event = records[0].get("dynamodb", {}).get("NewImage", {})
-    print("Event to process", event)
+    boards_table = dynamodb.Table(os.environ.get("BOARDS_TABLE"))
+
+    for record in records:
+        update_item(record, boards_table)
 
     return {}
+
+
+def update_item(record, table) -> None:
+    raw = record.get("dynamodb", {}).get("NewImage", {})
+    td = TypeDeserializer()
+    event = {k: td.deserialize(v) for k, v in raw.items()}
+
+    if event["eventType"] == "SCORE":
+        board_id = event["boardId"]
+        contender = event["contender"]
+        value = event["value"]
+        table.update_item(
+            Key={"boardId": board_id},
+            UpdateExpression=f"SET scores[{contender}] = "
+                             f"scores[{contender}] + :val",
+            ExpressionAttributeValues={":val": value}
+        )
