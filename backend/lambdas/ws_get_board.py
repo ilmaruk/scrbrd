f7bd5741322c05ed0c11764f71a3d6b30f7a3656
Ruggero Ferretti <ruggero.ferretti@audioanalytic.com>
2022-10-13 14:54:44 +0200

    WS GetBoard.

---
+++ backend/lambdas/ws_get_board.py
@@ -0,0 +1,32 @@
+import decimal
+import json
+import os
+
+import boto3
+from boto3.dynamodb.types import TypeDeserializer
+
+dynamodb = boto3.client("dynamodb")
+
+
+def lambda_handler(event, _context):
+    print("EVENT", event)
+
+    request_context = event.get("requestContext", {})
+    connection_id = request_context.get("connectionId")
+
+    board = {"a": 1}  # TODO: get from DB
+
+    api_gateway_management_api = boto3.client(
+        "apigatewaymanagementapi",
+        endpoint_url="https://" + os.environ.get("APIG_ENDPOINT"))
+    api_gateway_management_api.post_to_connection(
+        Data=json.dumps(board, cls=DecimalEncoder), ConnectionId=connection_id)
+
+    return {}
+
+
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, decimal.Decimal):
+            return str(o)
+        return super(DecimalEncoder, self).default(o)
